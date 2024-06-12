878236cc5fcf731dba452095a4424e3c5f00be82
dinggao.pan <dinggao.pan@horizon.cc>
2024-06-12 23:33:33 +0800

    arm: dts: hobot: feat [X5-784] Add evb-v2 dts support
    Signed-off-by: dinggao.pan <dinggao.pan@horizon.cc>
    Change-Id: I5d71c606920e71a85d2397f5e72cf0bd038d9cf4

--- include/dt-bindings/board/hb_board_id.h
+++ include/dt-bindings/board/hb_board_id.h
@@ -11,6 +11,7 @@
 
 #define HOBOT_X5_DVB_ID    0x0101
 #define HOBOT_X5_EVB_ID    0x0201
+#define HOBOT_X5_EVB_V2_ID 0x0202
 #define HOBOT_X5_RDK_ID    0x0301
 
 #if defined(CONFIG_HOBOT_X5_FPGA)
@@ -21,6 +22,8 @@
     #define HOBOT_X5_BOARD_ID HOBOT_X5_SVB_ID
 #elif defined(CONFIG_HOBOT_X5_EVB)
     #define HOBOT_X5_BOARD_ID HOBOT_X5_EVB_ID
+#elif defined(CONFIG_HOBOT_X5_EVB_V2)
+    #define HOBOT_X5_BOARD_ID HOBOT_X5_EVB_V2_ID
 #elif !defined(CONFIG_HOBOT_ADC_BTYPE)
     #define CONFIG_HOBOT_ADC_BTYPE
 #endif
