813898ee54e69a68e17bb267a1f52afd41c6ca95
ming.yu <ming.yu@hobot.cc>
2024-07-14 22:22:02 +0800

    efuse: feat [X5-521] Support write efuse in uboot
    Summary:
    efuse usage:
    1. efuse dump--->dump efuse information, inclue socid,customer public key, and all non-secure region
    2. efuse read--->read a efuse bank, efuse read [secure/nonsecure] [bank_index(hex)], example: efuse read secure 0x10, read secure bank16
    3. efuse write--->write a efuse bank, efuse write [secure/nonsecure] [bank_index(hex)] [value(hex)] [lock/unlock],example: efuse write secure 0x10 0x12345678 lock, write efuse secure bank16 to 0x123456 and lock this bank
    
    Signed-off-by: ming.yu <ming.yu@hobot.cc>
    Change-Id: I953dffd92730dbc357e121e51b39d52ecc096f55

--- arch/arm/include/asm/arch-x5/hb_efuse.h
+++ arch/arm/include/asm/arch-x5/hb_efuse.h
@@ -13,6 +13,7 @@ enum {
     MBEDTLS_OTP_USER_NON_SEC_REGION_OFFSET = 0x50U,
     MBEDTLS_OTP_USER_SOCID_HIGH_OFFSET     = 0x6CU,
     MBEDTLS_OTP_USER_SECUR_FLAG_OFFSET     = 0x78U,
+    MBEDTLS_OTP_USER_SEC_REGION_OFFSET     = 0x88U,
     MBEDTLS_OTP_USER_SEC_BL2PUB_OFFSET     = 0xBCU,
 };
 
@@ -38,4 +39,33 @@ enum {
 int hb_read_efuse(uint32_t offset, uint32_t size, char *output_buffer);
 int hb_get_socuid(uint32_t *socuid);
 bool is_secure_boot(void);
+
+enum efuse_type {
+	EFUSE_SECURE = 0,
+	EFUSE_NONSECURE
+};
+
+enum efuse_lock {
+	EFUSE_UNLOCK = 0,
+	EFUSE_LOCK
+};
+
+struct efuse_info {
+    enum efuse_type type;
+    uint32_t bank;
+    uint32_t value;
+    bool lock;
+};
+
+#define HB_EFUSE_PINMUX   0x31040008
+#define HB_EFUSE_PINMUX_OFFSET 12
+#define HB_EFUSE_PINMUX_MSAK    (0x3 << HB_EFUSE_PINMUX_OFFSET)
+#define HB_EFUSE_FUNC_GPIO      (1 << HB_EFUSE_PINMUX_OFFSET)
+#define HB_EFUSE_GPIO_DIR_REG  0x31000004
+#define HB_EFUSE_GPIO_DIR_OFFSET    7
+#define HB_EFUSE_GPIO_DIR_OUTPUT    (1 << HB_EFUSE_GPIO_DIR_OFFSET)
+#define HB_EFUSE_GPIO_VALUE_REG  0x31000000
+#define HB_EFUSE_GPIO_VALUE_OFFSET    7
+#define HB_EFUSE_GPIO_HIGH    (1 << HB_EFUSE_GPIO_VALUE_OFFSET)
+
 #endif /* __HOBOT_EFUSE_H__ */
--- arch/arm/mach-horizon/x5/x5_efuse.c
+++ arch/arm/mach-horizon/x5/x5_efuse.c
@@ -1,8 +1,10 @@
 #include <command.h>
 #include <common.h>
 #include <env.h>
+#include <asm/io.h>
 #include <errno.h>
 #include <tee.h>
+#include <linux/delay.h>
 #include <display_options.h>
 #include <asm/arch/hb_efuse.h>
 
@@ -15,6 +17,7 @@
 	}
 
 #define PTA_EFUSE_READ          0
+#define PTA_EFUSE_WRITE         1
 #define TEE_ERROR_ACCESS_DENIED 0xffff0001
 
 int hb_read_efuse(uint32_t offset, uint32_t size, char *output_buffer)
@@ -137,13 +140,275 @@ static int dump_efuse(void)
 	return ret;
 }
 
+static int is_valid_read_efuse_region(struct efuse_info *efuse)
+{
+	/* valid read region:
+		all non secure region
+		secure region[20:13] [0]
+	*/
+	if ((efuse->type == EFUSE_NONSECURE) && efuse->bank < 14)
+		return 1;
+
+	if ((efuse->type == EFUSE_SECURE) &&
+		((efuse->bank > 12 && efuse->bank < 21) || efuse->bank == 0))
+		return 1;
+
+	printf("READ efuse type:%d, bank:%d invalid\n", efuse->type, efuse->bank);
+	return 0;
+}
+
+static int hb_read_efuse_and_lock(struct efuse_info *efuse)
+{
+	int32_t ret = 0;
+	uint32_t offset = 0;
+	uint32_t size = 0;
+	uint32_t value = 0;
+
+	ret = is_valid_read_efuse_region(efuse);
+	if (ret == 0) {
+		return -1;
+	}
+	size = 4;
+	/* read bank value */
+	offset = efuse->bank * 4;
+	offset = efuse->type == EFUSE_SECURE ?
+	                        offset + MBEDTLS_OTP_USER_SEC_REGION_OFFSET : offset + MBEDTLS_OTP_USER_NON_SEC_REGION_OFFSET;
+	ret = hb_read_efuse(offset, size, (char *)&value);
+	if (ret) {
+		printf("read type:%s bank:%d failed\n",
+	            efuse->type == EFUSE_NONSECURE ? "NON-SECURE": "SECURE", efuse->bank);
+		return ret;
+	}
+	efuse->value = value;
+
+	/* read lock */
+	offset = efuse->type == EFUSE_SECURE ? MBEDTLS_OTP_USER_SEC_REGION_OFFSET : MBEDTLS_OTP_USER_NON_SEC_REGION_OFFSET;
+	ret = hb_read_efuse(offset, size, (char *)&value);
+	if (ret) {
+		printf("read type:%s  bank:%d lock status failed\n",
+	            efuse->type == EFUSE_NONSECURE ? "NON-SECURE": "SECURE", efuse->bank);
+		return ret;
+	}
+	efuse->lock = (value >> efuse->bank) & 0x1;
+	return ret;
+}
+
+static int hb_write_efuse(u32 offset, u32 value)
+{
+	int rc = CMD_RET_SUCCESS;
+	const struct tee_optee_ta_uuid uuid = PTA_EFUSE;
+	struct tee_open_session_arg session;
+	struct tee_invoke_arg invoke;
+	struct tee_param param[2];
+	struct udevice *tee_dev = NULL;
+	struct tee_shm *shm_val;
+
+	tee_dev = tee_find_device(NULL, NULL, NULL, NULL);
+	if (!tee_dev) {
+		rc = -ENODEV;
+		goto exit;
+	}
+
+	rc = tee_shm_alloc(tee_dev, sizeof(value), TEE_SHM_ALLOC, &shm_val);
+	if (rc) {
+		rc = -ENOMEM;
+		goto exit;
+	}
+
+	memcpy(shm_val->addr, &value, sizeof(value));
+
+	memset(&session, 0, sizeof(session));
+	tee_optee_ta_uuid_to_octets(session.uuid, &uuid);
+	if (tee_open_session(tee_dev, &session, 0, NULL)) {
+		rc = -ENXIO;
+		goto free_shm;
+	}
+
+	memset(&param, 0, sizeof(param));
+	param[0].attr          = TEE_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param[0].u.value.a     = offset;
+	param[1].attr          = TEE_PARAM_ATTR_TYPE_MEMREF_OUTPUT;
+	param[1].u.memref.shm  = shm_val;
+	param[1].u.memref.size = sizeof(value);
+
+	memset(&invoke, 0, sizeof(invoke));
+	invoke.func    = PTA_EFUSE_WRITE;
+	invoke.session = session.session;
+
+	rc = tee_invoke_func(tee_dev, &invoke, 2, param);
+	if (rc != 0)
+		printf("tee_invoke_func failed with error [0x%x]\n", rc);
+
+	if (invoke.ret) {
+		rc = invoke.ret;
+		if (invoke.ret == TEE_ERROR_ACCESS_DENIED)
+			printf("efuse region access denied\n");
+		else
+			printf("optee write efuse failed with error [0x%x]\n", invoke.ret);
+	}
+
+	tee_close_session(tee_dev, session.session);
+free_shm:
+	tee_shm_free(shm_val);
+exit:
+	return rc;
+}
+
+static int is_valid_write_efuse_region(struct efuse_info *efuse)
+{
+	/* valid read region:
+		non secure region[13:10]
+		secure region[20:13]
+	*/
+	if ((efuse->type == EFUSE_NONSECURE) &&
+		(efuse->bank > 9 && efuse->bank < 14))
+		return 1;
+
+	if ((efuse->type == EFUSE_SECURE) &&
+		(efuse->bank > 12 && efuse->bank < 21))
+		return 1;
+
+	printf("WRITE efuse type:%d, bank:%d invalid\n", efuse->type, efuse->bank);
+	return 0;
+}
+
+static void drobot_efuse_gpio_init(void)
+{
+	uint32_t value = 0;
+	/*
+	* 1, GPIO FUNC
+	* 2. OUTPUT DIRECTION
+	*/
+	value = readl(HB_EFUSE_PINMUX);
+	value &= ~HB_EFUSE_PINMUX_MSAK;
+	value |= HB_EFUSE_FUNC_GPIO;
+	writel(value, HB_EFUSE_PINMUX);
+
+	value = readl(HB_EFUSE_GPIO_DIR_REG);
+	value |= HB_EFUSE_GPIO_DIR_OUTPUT;
+	writel(value, HB_EFUSE_GPIO_DIR_REG);
+}
+
+static inline void drobot_efuse_gpio_high(void)
+{
+	uint32_t value = 0;
+
+	value = readl(HB_EFUSE_GPIO_VALUE_REG);
+	value |= HB_EFUSE_GPIO_HIGH;
+	writel(value, HB_EFUSE_GPIO_VALUE_REG);
+
+}
+
+static inline void drobot_efuse_gpio_low(void)
+{
+	uint32_t value = 0;
+
+	value = readl(HB_EFUSE_GPIO_VALUE_REG);
+	value &= ~HB_EFUSE_GPIO_HIGH;
+	writel(value, HB_EFUSE_GPIO_VALUE_REG);
+}
+
+
+static int is_valid_write_efuse_condition(struct efuse_info *efuse)
+{
+	int ret = 0;
+	struct efuse_info tmp = {0};
+
+	memcpy(&tmp, efuse, sizeof(tmp));
+	ret = hb_read_efuse_and_lock(&tmp);
+	if (ret) {
+		return 0;
+	}
+	if ((tmp.value != 0) || (tmp.lock != 0)) {
+		printf("type:%s bank:%d has been locked or burned\n", tmp.type == EFUSE_NONSECURE ? "NON-SECURE": "SECURE", tmp.bank);
+		return 0;
+	}
+	return 1;
+}
+
+static int hb_write_efuse_and_lock(struct efuse_info *efuse)
+{
+	int ret = 0;
+	uint32_t offset = 0;
+	uint32_t value = 0;
+
+	ret = is_valid_write_efuse_region(efuse);
+	if (ret == 0) {
+		return -1;
+	}
+
+	ret = is_valid_write_efuse_condition(efuse);
+	if (ret == 0) {
+		return -1;
+	}
+	if (efuse->value == 0) {
+		printf("efuse value is 0, skip\n");
+		return -1;
+	}
+	drobot_efuse_gpio_init();
+	drobot_efuse_gpio_high();
+	mdelay(1000);
+	/* update value */
+	offset = efuse->bank * 4;
+	offset = (efuse->type == EFUSE_SECURE) ?
+	          offset + MBEDTLS_OTP_USER_SEC_REGION_OFFSET : offset + MBEDTLS_OTP_USER_NON_SEC_REGION_OFFSET;
+	value = efuse->value;
+	ret = hb_write_efuse(offset, value);
+	if (ret) {
+		printf("write %s bank: %d failed\n",
+	           (efuse->type == EFUSE_SECURE) ? "SECURE" : "NON-SECURE",
+	           efuse->bank);
+		goto exit;
+	}
+
+	/* lock bank */
+	if (efuse->lock == 0) {
+		goto exit;
+	}
+	offset = (efuse->type == EFUSE_SECURE) ?
+	          MBEDTLS_OTP_USER_SEC_REGION_OFFSET : MBEDTLS_OTP_USER_NON_SEC_REGION_OFFSET;
+	value = 1 << efuse->bank;
+	ret = hb_write_efuse(offset, value);
+	if (ret) {
+		printf("lock %s bank: %d failed\n",
+	           (efuse->type == EFUSE_SECURE) ? "SECURE" : "NON-SECURE",
+	           efuse->bank);
+		goto exit;
+	}
+exit:
+	drobot_efuse_gpio_low();
+	return ret;
+}
+
+static uint32_t parse_efuse_type(char *type)
+{
+	if (strncmp(type, "secure", strlen("secure") + 1) == 0) {
+		return EFUSE_SECURE;
+	} else if (strncmp(type, "nonsecure", strlen("nonsecure") + 1) == 0) {
+		return EFUSE_NONSECURE;
+	}
+
+	printf("parse efuse type failed,valid efuse type: 'secure' or 'nonsecure'\n");
+	return -1;
+}
+
+static uint32_t parse_efuse_lock(char *lock)
+{
+	if (strncmp(lock, "lock", strlen("lock") + 1) == 0) {
+		return EFUSE_LOCK;
+	} else if (strncmp(lock, "unlock", strlen("unlock") + 1) == 0) {
+		return EFUSE_UNLOCK;
+	}
+
+	printf("parse efuse lock status failed,valid efuse type: 'lock' or 'unlock'\n");
+	return -1;
+}
+
 static int do_optee_efuse(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	const char *cmd;
-	uint32_t offset;
-	uint32_t size;
-	char output_buffer[256];
 	int ret;
+	struct efuse_info info = {0};
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
@@ -153,24 +418,49 @@ static int do_optee_efuse(struct cmd_tbl *cmdtp, int flag, int argc, char *const
 	if (strcmp(cmd, "read") == 0) {
 		if (argc < 4)
 			return CMD_RET_USAGE;
-		offset = hextoul(argv[2], NULL);
-		size   = hextoul(argv[3], NULL) * 4;
-		ret = hb_read_efuse(offset, size, output_buffer);
-		if (ret == CMD_RET_SUCCESS)
-		{
-			printf("otpee read efuse values:\n");
-			print_buffer(offset, output_buffer, 4, size / 4, 1);
+		ret = parse_efuse_type(argv[2]);
+		if (ret < 0) {
+			return -1;
+		}
+		info.type = ret;
+		info.bank = hextoul(argv[3], NULL);
+		ret = hb_read_efuse_and_lock(&info);
+		if (ret == 0) {
+			printf("value:0x%x\n", info.value);
+			printf("lock:%s\n", info.lock == 1 ? "true" : "false");
 		}
 		return ret;
-	}
-	if (strcmp(cmd, "dump") == 0) {
+	} else if (strcmp(cmd, "dump") == 0) {
 		if (argc != 2)
 			return CMD_RET_USAGE;
 		return dump_efuse();
+	} else if (strcmp(cmd, "write") == 0) {
+		if (argc < 6)
+			return CMD_RET_USAGE;
+		ret = parse_efuse_type(argv[2]);
+		if (ret < 0) {
+			return -1;
+		}
+		info.type = ret;
+		info.bank = hextoul(argv[3], NULL);
+		info.value = hextoul(argv[4], NULL);
+		ret = parse_efuse_lock(argv[5]);
+		if (ret < 0) {
+			return -1;
+		}
+		info.lock = ret;
+		ret = hb_write_efuse_and_lock(&info);
+		if (ret) {
+			printf("write %s bank:0x%x failed\n", info.type == EFUSE_SECURE ? "SECURE" : "NONSECURE", info.bank);
+		} else {
+			printf("write %s bank:0x%x success\n", info.type == EFUSE_SECURE ? "SECURE" : "NONSECURE", info.bank);
+		}
+		return ret;
 	}
 	return CMD_RET_USAGE;
 }
 
-U_BOOT_CMD(efuse, 4, 1, do_optee_efuse, "Read/Dump efuse access via optee",
-		   "read [offset] [dword_count] - read 'dword_count' banks\n"
+U_BOOT_CMD(efuse, 6, 1, do_optee_efuse, "Read/Dump efuse access via optee",
+		   "read [type] [bank] - efuse read 'type' 'banks'\n"
+		   "efuse write [type] [bank] [value] [lock]- efuse write 'type' 'banks' 'value' 'lock'\n"
 		   "efuse dump - dump all banks\n");
