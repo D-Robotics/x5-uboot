3919a7afe1751e0c785ec9809b403938c64e79a1
xin03.liu <xin03.liu@horizon.cc>
2025-01-17 13:10:54 +0800

    fix(socinfo): Set board_id from 0x0301 to 0x301
    Signed-off-by: xin03.liu <xin03.liu@horizon.cc>

--- cmd/socinfo.c
+++ cmd/socinfo.c
@@ -36,7 +36,7 @@ static int hb_dtb_property_config(int offset, char *prop, int value)
 		return 1;
 	}
 
-	snprintf(node_data, sizeof(node_data), "0x%04x", value);
+	snprintf(node_data, sizeof(node_data), "%x", value);
 	len = strlen(node_data) + 1;
 
 	ret = fdt_setprop(hb_dtb, offset, prop, node_data, len);
